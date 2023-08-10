78cac80a4c2b612e44d7e8ec2d3ffedd2ba4564e
juanmolera <juan.molera@gmail.com>
2023-08-10 21:15:46 +0200

    .

--- streamlit/pages/2_👀_Viz.py
+++ streamlit/pages/2_👀_Viz.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 # My functions
-from src import stfunctions as fu
+from src import streamlit_functions as fu
 
 # Data manipulation
 import pandas as pd
@@ -14,37 +14,38 @@ import plotly.express as px
 from streamlit_keplergl import keplergl_static
 from keplergl import KeplerGl
 
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed", page_title='Viz', page_icon="👀")
+st.set_page_config(layout = 'wide', initial_sidebar_state = 'collapsed', page_title = 'Viz', page_icon = '👀')
 
 st.markdown('# Viz')
 
-options = st.selectbox('Which city do you want to visualize?', ['Choose an option'] + ['Madrid', 'Porto', 'Lisbon'])
+cities = ['Madrid', 'Porto', 'Lisbon']
 
-# Madrid
-if options == 'Madrid':
+city = st.selectbox('Which city do you want to visualize?', ['Choose an option'] + cities)
+
+if city in cities:
 
     # Pandas
     st.markdown('#### Airbnb data example:')
-    df_madrid = pd.read_csv('../data/kepler/airbnb_madrid.csv')
-    st.table(df_madrid.sample(1))
+    df = pd.read_csv(f'../data/kepler/airbnb_{city.lower()}.csv')
+    st.table(df.sample(1))
 
     # District section
-    st.markdown('#### Airbnbs per district in Madrid:')
+    st.markdown(f'#### Airbnbs per district in {city}:')
 
     # Kepler map
-    map1 = KeplerGl(height=400, data={'data1': df_madrid})#, config=config)
-    keplergl_static(map1)
+    config = {}
+    keplergl_static(fu.kepler_map_viz(df, config))
 
     # Histogram
-    st.plotly_chart(fu.histogram_viz(df_madrid, 'District'), use_container_width=True)
+    st.plotly_chart(fu.histogram_viz(df, 'District'), use_container_width=True)
 
     # District
-    options2 = st.selectbox('Which district data do you want to visualize?', ['Choose an option'] + sorted(df_madrid['District'].unique().tolist()))
+    district = st.selectbox('Which district data do you want to visualize?', ['Choose an option'] + sorted(df['District'].unique().tolist()))
     
-    if options2 in df_madrid['District'].unique().tolist():
+    if district in df['District'].unique().tolist():
 
         # District information in detail
-        st.markdown(f'#### {options2} district:')
+        st.markdown(f'#### {district} information in detail:')
 
         col1, col2, col3 = st.columns(3)
 
@@ -61,19 +62,19 @@ if options == 'Madrid':
         with col3:
 
             st.markdown('Total Airbnbs')
-            st.metric(label='', value = df_madrid[df_madrid['District'] == options2].value_counts().sum())
+            st.metric(label='', value = df[df['District'] == district].value_counts().sum())
 
         # Neighbourhood
-        st.markdown(f'#### Airbnbs in {options2} neighbourhoods:')
+        st.markdown(f'#### Airbnbs in {district} neighbourhoods:')
         
-        st.plotly_chart(fu.histogram_viz_with_filter(df_madrid, 'District', 'Neighbourhood', options2), use_container_width=True)
+        st.plotly_chart(fu.histogram_with_filter_viz(df, 'District', 'Neighbourhood', district), use_container_width=True)
 
-        options3 = st.selectbox('Which neighbourhood data do you want to visualize?', ['Choose an option'] + sorted(df_madrid['Neighbourhood'][df_madrid['District'] == options2].unique().tolist()))
+        neighbourhood = st.selectbox('Which neighbourhood data do you want to visualize?', ['Choose an option'] + sorted(df['Neighbourhood'][df['District'] == district].unique().tolist()))
         
-        if options3 in df_madrid['Neighbourhood'].unique().tolist():
+        if neighbourhood in df['Neighbourhood'].unique().tolist():
 
             # Neighbourhood information in detail
-            st.markdown(f'#### {options3} neighbourhood:')
+            st.markdown(f'#### {neighbourhood} neighbourhood:')
 
             col1, col2, col3 = st.columns(3)
 
@@ -90,48 +91,4 @@ if options == 'Madrid':
             with col3:
 
                 st.markdown('Total Airbnbs')
-                st.metric(label='', value = df_madrid[df_madrid['Neighbourhood'] == options3].value_counts().sum())
-
-# Porto
-elif options == 'Porto':
-
-    # Pandas
-    st.markdown('#### Airbnb data example:')
-    df_porto = pd.read_csv('../data/kepler/airbnb_porto.csv')
-    st.table(df_porto.sample(1))
-
-    # District
-    st.markdown('#### Airbnbs per district in Porto:')
-    fig1 = px.histogram(df_porto, x = df_porto['District'])
-    fig1.update_xaxes(categoryorder = 'total descending')
-    fig1.update_xaxes(tickangle = 90)
-    st.plotly_chart(fig1, use_container_width=True)
-
-    # Neighbourhood
-    st.markdown('#### Airbnbs per neighbourhood in Porto:')
-    fig2 = px.histogram(df_porto, x = df_porto['Neighbourhood'])
-    fig2.update_xaxes(categoryorder = 'total descending')
-    fig2.update_xaxes(tickangle = 90)
-    st.plotly_chart(fig2, use_container_width=True)
-
-# Lisbon
-elif options == 'Lisbon':
-
-    # Pandas
-    st.markdown('#### Airbnb data example:')
-    df_lisbon = pd.read_csv('../data/kepler/airbnb_lisbon.csv')
-    st.table(df_lisbon.sample(1))
-
-    # District
-    st.markdown('#### Airbnbs per district in Lisbon:')
-    fig1 = px.histogram(df_lisbon, x = df_lisbon['District'])
-    fig1.update_xaxes(categoryorder = 'total descending')
-    fig1.update_xaxes(tickangle = 90)
-    st.plotly_chart(fig1, use_container_width=True)
-
-    # Neighbourhood
-    st.markdown('#### Airbnbs per neighbourhood in Lisbon:')
-    fig2 = px.histogram(df_lisbon, x = df_lisbon['Neighbourhood'])
-    fig2.update_xaxes(categoryorder = 'total descending')
-    fig2.update_xaxes(tickangle = 90)
-    st.plotly_chart(fig2, use_container_width=True)
+                st.metric(label='', value = df[df['Neighbourhood'] == neighbourhood].value_counts().sum())
--- streamlit/src/streamlit_functions.py
+++ streamlit/src/streamlit_functions.py
@@ -1,4 +1,9 @@
+# Data visualization
 import plotly.express as px
+
+# Kepler.gl maps
+from streamlit_keplergl import keplergl_static
+from keplergl import KeplerGl
 
 # Histogram
 def histogram_viz(df, option):
@@ -10,10 +15,17 @@ def histogram_viz(df, option):
     return fig
 
 # Filtered histogram
-def histogram_viz_with_filter(df, option, option2, filter):
+def histogram_with_filter_viz(df, option, option2, filter):
 
     fig = px.histogram(df, x = df[option2][df[option] == filter])
     fig.update_xaxes(categoryorder = 'total descending')
     fig.update_xaxes(tickangle = 90)
 
     return fig
+
+# Kepler.gl map viz
+def kepler_map_viz(df, config):
+
+    map = KeplerGl(height=400, data={'data1': df}, config=config)
+
+    return map
