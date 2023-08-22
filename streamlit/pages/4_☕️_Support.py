9d8ff87de25d71b74e275621648f72fb053b0e84
juanmolera <juan.molera@gmail.com>
2023-08-22 18:44:23 +0200

    .

---
+++ streamlit/pages/4_☕️_Support.py
@@ -0,0 +1,14 @@
+# Streamlit
+import streamlit as st
+
+from PIL import Image
+
+st.set_page_config(layout = 'wide', initial_sidebar_state = 'collapsed', page_title = 'Support', page_icon = '☕️')
+
+# Title
+st.markdown('# Support')
+
+# Coffee
+st.markdown('## Hey, give me your [money](https://www.buymeacoffee.com/juanmolera)')
+image = Image.open('images/fry-shut-up-and-take-my-money.png')
+st.image(image, use_column_width=True)
--- streamlit/pages/5_🐷_Drafts.py
+++
@@ -1,6 +0,0 @@
-# Streamlit
-import streamlit as st
-
-# Kepler.gl maps
-from streamlit_keplergl import keplergl_static
-from keplergl import KeplerGl
