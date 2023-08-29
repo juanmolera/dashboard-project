96ead9bacf72ceae75b220b1e1a1b4ed4d11d752
juanmolera <juan.molera@gmail.com>
2023-08-30 01:13:26 +0200

    .

--- streamlit/🏠Main.py
+++ streamlit/🏠Main.py
@@ -9,17 +9,29 @@ with open('css/style.css') as f:
         f'<style>{f.read()}</style>',
         unsafe_allow_html=True)
 
-st.markdown('# Tourist dictatorship?')
+col1, col2, col3 = st.columns(3)
 
-image = Image.open('images/cover2.png')
-st.image(image, use_column_width=True)
+with col1:
 
-st.markdown('## Introduction')
-st.write('This is my second project of the Ironhack data analytics bootcamp and it is focused on the analysis of the impact of Airbnb over different population parameters in Madrid.')
+    pass
+
+with col2:
+
+    st.markdown('# Tourist dictatorship?')
+
+    image = Image.open('images/cover2.png')
+    st.image(image, use_column_width=True)
+
+with col3:
+
+    pass
+
+#st.markdown('## Introduction')
+#st.write('This is my second project of the Ironhack data analytics bootcamp and it is focused on the analysis of the impact of Airbnb over different population parameters in Madrid.')
 
 
-st.markdown('## Motivation & Objectives')
-st.write('- Initial situation, Portugal case heatmap vs. Spain with population data and rental flats numbers')
-st.write('- Commuting de barrio según Airbnbs')
-st.write('- % Inmigrandes en zonas más pobladas de Airbnb')
-st.write('- Incremento en el precio del alquiler (ójala)')
+#st.markdown('## Motivation & Objectives')
+#st.write('- Initial situation, Portugal case heatmap vs. Spain with population data and rental flats numbers')
+#st.write('- Commuting de barrio según Airbnbs')
+#st.write('- % Inmigrandes en zonas más pobladas de Airbnb')
+#st.write('- Incremento en el precio del alquiler (ójala)')
